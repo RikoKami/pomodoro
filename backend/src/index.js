43d96fa34af074457896156620296b3c86b9cd99
Larissa <larissa.silva@cubos.io>
2020-02-03 15:15:13 -0200

    fix env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const env = require("dotenv/config");
 
 const app = express();
 
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cymly.mongodb.net/test?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cymly.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .catch(err => console.log(err));
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log(`Listening at 3333`);
+});
