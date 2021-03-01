eb228946e7f74dff0ba3fb6ef9ba4a048552a0f0
Riko <orglary@gmail.com>
2021-02-28 21:20:26 -0300

    fix: prettier

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 import s from "./experienceBar.module.scss";
 
 export const ExperienceBar = () => {
-    return (
-        <div className={s.experienceBar}>
-            <span>0 xp</span>
-            <div className={s.current}>
-                <div style={{ width: '50%'}} />
-                <span className={s.currentExp} style={{left: "50%"}}>
-                    300px
-                </span>
-            </div>
-            <span>600px</span>
-        </div>
-    )
-}
+	return (
+		<div className={s.experienceBar}>
+			<span>0 xp</span>
+			<div className={s.current}>
+				<div style={{ width: "50%" }} />
+				<span className={s.currentExp} style={{ left: "50%" }}>
+					300px
+				</span>
+			</div>
+			<span>600px</span>
+		</div>
+	);
+};
