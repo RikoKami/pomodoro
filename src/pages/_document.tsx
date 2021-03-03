aa87a314a6492032213e268232e1c269b7864c65
Riko <orglary@gmail.com>
2021-03-03 20:00:49 -0300

    feat: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
@@ -6,6 +5,7 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel="shortcut icon" href="favicon.png" type="image/png" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
