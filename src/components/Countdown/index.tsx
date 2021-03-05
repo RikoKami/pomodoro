a9a8cb4fdced2ee91f9f6e3e733b348e9843780e
Riko <orglary@gmail.com>
2021-03-04 21:58:44 -0300

    feat: add button start countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -2,16 +2,19 @@ import s from "./countdown.module.scss"
 
 export const Countdown = () => {
   return (
-    <div className={s.countdown}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <>
+      <div className={s.countdown}>
+        <div>
+          <span>2</span>
+          <span>5</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>0</span>
+          <span>0</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
-    </div>
+      <button type="button" className={s.countdownBtn}>Iniciar um ciclo</button>  
+    </>
   )
 }
