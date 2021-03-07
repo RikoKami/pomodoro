b920417edd177a5d0badeb8abee44add28526fc2
Riko <orglary@gmail.com>
2021-03-06 22:35:00 -0300

    feat: adding function to start countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,20 +1,43 @@
+import { useEffect, useState } from "react";
 import s from "./countdown.module.scss"
 
 export const Countdown = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("")
+  const [secondsLeft, secondsRight] = String(seconds).padStart(2, "0").split("")
+
+  const startCountDown = () => {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if(active && time > 0){
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  },[active, time])
+
+
   return (
     <>
       <div className={s.countdown}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
         </div>
       </div>
-      <button type="button" className={s.countdownBtn}>Iniciar um ciclo</button>  
+      <button type="button" className={s.countdownBtn} onClick={startCountDown}>Iniciar um ciclo</button>  
     </>
   )
 }
