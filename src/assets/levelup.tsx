79f6fe632ff9625ba430a3b94c7403681caebcae
Riko <orglary@gmail.com>
2021-03-08 22:12:20 -0300

    feat: removing the width and height of the LevelUp component

--- src/assets/levelup.tsx
+++ src/assets/levelup.tsx
@@ -4,8 +4,6 @@ export const LevelUp = ({ className }: React.SVGProps<SVGSVGElement>) => {
 	return (
 		<svg
 			className={className}
-			width="14"
-			height="16"
 			viewBox="0 0 14 16"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
