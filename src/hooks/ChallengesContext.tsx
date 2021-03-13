f9780dcde9d5eb04db79de99752d762d4cd8b8b1
Riko <orglary@gmail.com>
2021-03-13 20:45:16 -0300

    feat: setting values that will be used by the context

--- src/hooks/ChallengesContext.tsx
+++ src/hooks/ChallengesContext.tsx
@@ -1,18 +1,31 @@
 import { createContext, ReactNode, useState } from 'react';
 
-export const ChallengesContext = createContext({});
-
 interface ChallengesProviderProps {
   children: ReactNode;
 }
+interface ChallengesContextDate {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextDate);
 
 export const ChallengesProvider = ({children}: ChallengesProviderProps) => {
   const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   const levelUp = () => setLevel(level + 1)
 
+  const startNewChallenge = () => {
+    console.log('New Challenge')
+  }
+
   return (
-    <ChallengesContext.Provider value={{ level, levelUp}}>
+    <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge}}>
       {children}
     </ChallengesContext.Provider>
   )
