8b2f87d593ee4378deb681f430264f16e157c17e
Riko <orglary@gmail.com>
2021-03-14 22:31:48 -0300

    feat: inserting logic to start a new challenge randomly by context

--- src/hooks/ChallengesContext.tsx
+++ src/hooks/ChallengesContext.tsx
@@ -4,12 +4,19 @@ import challenges from "../../challenges.json";
 interface ChallengesProviderProps {
   children: ReactNode;
 }
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 interface ChallengesContextDate {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
   levelUp: () => void;
   startNewChallenge: () => void;
+  activeChallenge: Challenge; 
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextDate);
@@ -19,14 +26,18 @@ export const ChallengesProvider = ({children}: ChallengesProviderProps) => {
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
   const levelUp = () => setLevel(level + 1)
 
   const startNewChallenge = () => {
-    console.log('New Challenge')
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+    setActiveChallenge(challenge);
   }
 
   return (
-    <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge}}>
+    <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge, activeChallenge }}>
       {children}
     </ChallengesContext.Provider>
   )
