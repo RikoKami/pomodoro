3ebac5e3e3d753402cdefdc079ec4e4e4c677e6c
Riko <orglary@gmail.com>
2021-03-17 22:11:55 -0300

    feat: showing total challenges completed

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../../hooks/ChallengesContext";
 import s from "./completedChallenges.module.scss";
 
 export const CompletedChallenges = () => {
+	const { challengesCompleted } = useContext(ChallengesContext);
+
 	return (
 		<div className={s.completedChallenges}>
 			<span>Desafios completos</span>
-			<span>5</span>
+			<span>{challengesCompleted}</span>
 		</div>
 	);
 };
