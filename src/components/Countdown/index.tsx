665ddf1737e9fb4a8536c6570e542d2d6bb2c3a2
Riko <orglary@gmail.com>
2021-03-20 01:36:16 -0300

    feat: moving countdown logic to its context

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,40 +1,19 @@
-import { useContext, useEffect, useState } from "react";
-import { ChallengesContext } from "../../hooks/ChallengesContext";
+import { useContext } from "react";
+import { CountdownContext } from "../../hooks/CountdownContext";
 import s from "./countdown.module.scss";
 
-let countdownTimeout: NodeJS.Timeout;
-
 export const Countdown = () => {
-	const { startNewChallenge } = useContext(ChallengesContext);
-	const [time, setTime] = useState(0.1 * 60);
-	const [isActive, setIsActive] = useState(false);
-	const [hasFinished, setHasFinished] = useState(false);
-
-	const minutes = Math.floor(time / 60);
-	const seconds = time % 60;
-
+	const {
+		minutes,
+		seconds,
+		hasFinished,
+		handleCountdown,
+		isActive,
+	} = useContext(CountdownContext);
 	const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
 	const [secondsLeft, secondsRight] = String(seconds)
 		.padStart(2, "0")
 		.split("");
-
-	const handleCountdown = () => {
-		setIsActive(!isActive);
-		clearTimeout(countdownTimeout);
-		setTime(0.1 * 60);
-	};
-
-	useEffect(() => {
-		if (isActive && time > 0) {
-			countdownTimeout = setTimeout(() => {
-				setTime(time - 1);
-			}, 1000);
-		} else if (isActive && time === 0) {
-			setHasFinished(true);
-			setIsActive(false);
-			startNewChallenge();
-		}
-	}, [isActive, time]);
 
 	return (
 		<>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import React from "react";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CountdownProvider } from "../hooks/CountdownContext";
 import s from "../styles/pages/home.module.scss";
 
 export default function Home() {
@@ -13,14 +15,17 @@ export default function Home() {
 				<title>Início | Pomo</title>
 			</Head>
 			<ExperienceBar />
-			<section className={s.column}>
-				<div>
-					<Profile />
-					<CompletedChallenges />
-					<Countdown />
-				</div>
-				<ChallengeBox />
-			</section>
+
+			<CountdownProvider>
+				<section className={s.column}>
+					<div>
+						<Profile />
+						<CompletedChallenges />
+						<Countdown />
+					</div>
+					<ChallengeBox />
+				</section>
+			</CountdownProvider>
 		</div>
 	);
 }
