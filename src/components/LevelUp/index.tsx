2372b6d2b02114b200aa0902979e46067648f7d3
Riko <orglary@gmail.com>
2021-03-27 21:14:31 -0300

    feat: level up modal

---
+++ src/components/LevelUp/index.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Close } from "../../assets/close";
+import { ChallengesContext } from "../../hooks/ChallengesContext";
+import s from "./levelUp.module.scss";
+
+export const LevelUpModal = () => {
+	const { level } = useContext(ChallengesContext);
+
+	return (
+		<div className={s.overlay}>
+			<div className={s.modal}>
+				<header>
+					<span>{level}</span>
+				</header>
+
+				<strong>Parabéns!</strong>
+				<p>Você alcançou um novo level.</p>
+
+				<button type="button">
+					<Close />
+				</button>
+			</div>
+		</div>
+	);
+};
